8db1a58513fad72b90b5b7ee5b27354ce6ae767e
Felipe-Gs <felipegs2905@gmail.com>
2024-07-15 20:45:35 -0300

    feat: added serviced property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ interface Client {
   nome: string;
   contato: string;
   tipoAtendimento: string;
+  atendido: boolean;
 }
 
 @Component({
@@ -16,29 +17,45 @@ export class AppComponent {
     nome: '',
     contato: '',
     tipoAtendimento: '',
+    atendido: false,
   };
 
   filaClientes: Client[] = [
     {
+      nome: 'Marcos goes',
+      contato: 'marcos@gmail.com',
+      tipoAtendimento: 'PCD',
+      atendido: false,
+    },
+    {
       nome: 'Felipe Silva',
       contato: 'contatofleipe@gmail.com',
-      tipoAtendimento: 'Prioritário',
+      tipoAtendimento: 'gerais',
+      atendido: false,
     },
     {
       nome: 'Silva Pedro',
       contato: 'Silva@gmail.com',
       tipoAtendimento: 'Gestantes',
+      atendido: false,
     },
     {
       nome: 'Lucas sabino',
       contato: 'sabino@gmail.com',
       tipoAtendimento: 'Idosos',
+      atendido: false,
     },
-    { nome: 'Ana Souza', contato: 'ana@gmail.com', tipoAtendimento: 'PCD' },
+    {
+      nome: 'Ana Souza',
+      contato: 'ana@gmail.com',
+      tipoAtendimento: 'PCD',
+      atendido: false,
+    },
     {
       nome: 'Carlos Lima',
       contato: 'carlos@gmail.com',
       tipoAtendimento: 'Encaminhados pelo médico',
+      atendido: false,
     },
   ];
 
@@ -48,25 +65,38 @@ export class AppComponent {
     this.novoCliente = {
       nome: '',
       contato: '',
-      tipoAtendimento: 'Normal',
+      tipoAtendimento: '',
+      atendido: false,
     };
   }
 
+  // Exemplo: Se alguém do tipo Encaminhados pelo médico(nível
+  // dois) for inserido na fila ele será prioridade e estará acima na
+  // listagem sobre todos do tipo Casos gerais (nível 3).
   ordenarFila() {
+    const prioridades: { [key: string]: number } = {
+      Gestantes: 3,
+      Idosos: 3,
+      PCD: 3,
+      Encaminhados: 2,
+      gerais: 1,
+    };
+
     this.filaClientes.sort((a, b) => {
-      if (
-        a.tipoAtendimento === 'Prioritário' &&
-        b.tipoAtendimento === 'Normal'
-      ) {
-        return -1;
-      } else if (
-        a.tipoAtendimento === 'Normal' &&
-        b.tipoAtendimento === 'Prioritário'
-      ) {
-        return 1;
-      } else {
-        return 0;
-      }
+      const prioridadeA = prioridades[a.tipoAtendimento];
+      const prioridadeB = prioridades[b.tipoAtendimento];
+
+      if (prioridadeA === undefined) return 1;
+      if (prioridadeB === undefined) return -1;
+
+      return prioridadeB - prioridadeA;
     });
   }
+
+  // Somente o próximo cliente a ser atendido recebe ao lado do seu
+  // nome na lista um botão (ATENDIDO), que o remove da
+  // listagem(fila).Seria interessante conseguir visualizar as pessoas
+  // que já foram atendidas
+
+  visualizarAtendidos() {}
 }
