8fa35c16d42192b392cbc258747f4328d0ed4f7e
Felipe-Gs <felipegs2905@gmail.com>
2024-07-15 19:49:49 -0300

    feat: repository started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
 
+interface Client {
+  nome: string;
+  contato: string;
+  tipoAtendimento: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'DesafioFilaPcdoc';
+  novoCliente: Client = {
+    nome: '',
+    contato: '',
+    tipoAtendimento: 'Normal',
+  };
+
+  filaClientes: Client[] = [];
+
+  adicionarCliente() {
+    this.filaClientes.push({ ...this.novoCliente });
+    this.ordenarFila();
+    this.novoCliente = {
+      nome: '',
+      contato: '',
+      tipoAtendimento: 'Normal',
+    };
+  }
+
+  ordenarFila() {
+    this.filaClientes.sort((a, b) => {
+      if (
+        a.tipoAtendimento === 'Prioritário' &&
+        b.tipoAtendimento === 'Normal'
+      ) {
+        return -1;
+      } else if (
+        a.tipoAtendimento === 'Normal' &&
+        b.tipoAtendimento === 'Prioritário'
+      ) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
 }
