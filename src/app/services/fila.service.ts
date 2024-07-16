58e7148cf2b0b7fa585384a80e579de49f3cae1c
Felipe-Gs <felipegs2905@gmail.com>
2024-07-16 14:00:22 -0300

    feat: criado service para usar as requisiicoes da api

---
+++ src/app/services/fila.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilaService {
+  private baseUrl = 'http://localhost:8080/users';
+
+  constructor(private http: HttpClient) {}
+
+  adicionarDadosMockados(): Observable<string> {
+    return this.http.post<string>(`${this.baseUrl}/add`, {});
+  }
+
+  testeDeConexao(): Observable<string> {
+    return this.http.get(`${this.baseUrl}/test`, { responseType: 'text' });
+  }
+
+  adicionarUsuario(user: any): Observable<string> {
+    return this.http.post<string>(`${this.baseUrl}/addDados`, user);
+  }
+
+  buscarUsuarios(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}`);
+  }
+}
